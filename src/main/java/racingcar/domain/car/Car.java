25cb560949d01a3cdc2e3f01fd1218740beb0986
kcw0360 <kcw0360@naver.com>
2023-11-20 20:23:51 +0900

    feat(Car): 포지션 변경하는 메소드 작성

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -2,11 +2,22 @@ package racingcar.domain.car;
 
 public class Car {
 
+    private static final int MOVE_MIN_NUMBER = 4;
     private final String name;
     private int position;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void move(int number) {
+        if (isMove(number)) {
+            position++;
+        }
+    }
+
+    private boolean isMove(int number) {
+        return number >= MOVE_MIN_NUMBER;
     }
 
     public String getName() {
