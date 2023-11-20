d51c3a930b3b28758cb249da8646655c9667be08
kcw0360 <kcw0360@naver.com>
2023-11-20 18:55:13 +0900

    feat(InputView): 입력값 검증 적용

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,12 +3,19 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.util.validator.AttemptsNumbersValidator;
+import racingcar.util.validator.CarNamesValidator;
+import racingcar.util.validator.Constant;
+import racingcar.util.validator.Validator;
 
 public class InputView {
 
-    private static final String DELIMITER = ",";
+    private final Validator namesValidator;
+    private final Validator attempsValidator;
 
     private InputView() {
+        this.namesValidator = new CarNamesValidator();
+        this.attempsValidator = new AttemptsNumbersValidator();
     }
 
     public static InputView getInstance() {
@@ -17,13 +24,15 @@ public class InputView {
 
     public List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        List<String> carNames = Arrays.asList(input().split(DELIMITER));
-        return carNames;
+        String input = input();
+        namesValidator.validate(input);
+        return Arrays.asList(input.split(Constant.DELIMITER_COMMA));
     }
 
     public int readAttemptsNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
         String input = input();
+        attempsValidator.validate(input);
         return Integer.parseInt(input);
     }
 
