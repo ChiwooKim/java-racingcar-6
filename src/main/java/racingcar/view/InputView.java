a7aadac2df429935d16ef2c6556e16c4334ec7a4
kcw0360 <kcw0360@naver.com>
2023-11-20 19:19:19 +0900

    feat(InputView): 출력메시지 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -23,14 +23,12 @@ public class InputView {
     }
 
     public List<String> readCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = input();
         namesValidator.validate(input);
         return Arrays.asList(input.split(Constant.DELIMITER_COMMA));
     }
 
     public int readAttemptsNumber() {
-        System.out.println("시도할 회수는 몇회인가요?");
         String input = input();
         attempsValidator.validate(input);
         return Integer.parseInt(input);
