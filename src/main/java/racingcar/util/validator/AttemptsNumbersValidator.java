7f8b897ec95347a767ac903181bc8c6df02e68bc
kcw0360 <kcw0360@naver.com>
2023-11-20 18:51:10 +0900

    feat(AttemptsNumbersValidator): 시도 횟수 숫자 범위 검증하는 메소드 작성

--- src/main/java/racingcar/util/validator/AttemptsNumbersValidator.java
+++ src/main/java/racingcar/util/validator/AttemptsNumbersValidator.java
@@ -4,16 +4,26 @@ import java.util.regex.Pattern;
 
 public class AttemptsNumbersValidator extends Validator{
 
+    private static final int ATTEMPTS_MIN_RANGE = 1;
+    private static final int ATTEMPTS_MAX_RANGE = 20;
     private final Pattern numberValidatePattern = Pattern.compile("^[0-9]{1,2}$");
 
     @Override
     public void validate(String input) {
         validateNumber(input);
+        validateNumberRange(input);
+    }
+
+    private void validateNumberRange(String input) {
+        int number = Integer.parseInt(input);
+        if (number < ATTEMPTS_MIN_RANGE || number > ATTEMPTS_MAX_RANGE) {
+            throw new IllegalArgumentException("1 이상 20 이하의 숫자만 입력해주세요.");
+        }
     }
 
     private void validateNumber(String input) {
         if (!numberValidatePattern.matcher(input).matches()) {
-            throw new IllegalArgumentException("1 ~ 20 회만 가능합니다.");
+            throw new IllegalArgumentException("1 이상 20 이하의 숫자만 입력해주세요.");
         }
     }
 }
