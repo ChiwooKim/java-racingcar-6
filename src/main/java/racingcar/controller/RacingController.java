73dcca2bb794e7b7509ef7364d0cb03d01fdf49d
kcw0360 <kcw0360@naver.com>
2023-11-20 20:36:47 +0900

    feat(RacingController): 메인 로직 작성 (실행)

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,20 +1,43 @@
 package racingcar.controller;
 
 import java.util.List;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
+import racingcar.util.RandomNumberGenerator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingController {
 
     private final InputView inputView;
+    private final OutputView outputView;
+    private final Cars cars;
 
-    public RacingController(InputView inputView) {
+    public RacingController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
+        this.outputView = outputView;
+        this.cars = new Cars();
     }
 
     public void play() {
+        outputView.printCarNamesInput();
         List<String> carNames = inputView.readCarNames();
+        outputView.printAttemptsInput();
         int attemptsNumber = inputView.readAttemptsNumber();
 
+        for (String carName : carNames) {
+            cars.addCar(new Car(carName));
+        }
+
+        int count = 0;
+        while (count < attemptsNumber) {
+            for (Car car : cars.getCars()) {
+                car.move(RandomNumberGenerator.createRandomNumber());
+                outputView.printResult(car);
+            }
+            outputView.printSpace();
+            count++;
+        }
 
     }
 }
