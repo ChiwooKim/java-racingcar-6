2e59a6b71d2a10e775f002e3700adcb403d34343
kcw0360 <kcw0360@naver.com>
2023-11-20 22:46:01 +0900

    feat(CarNamesValidator): 에러메시지 enum으로 관리

--- src/main/java/racingcar/util/validator/CarNamesValidator.java
+++ src/main/java/racingcar/util/validator/CarNamesValidator.java
@@ -25,13 +25,28 @@ public class CarNamesValidator extends Validator {
 
     private void validateNamePattern(String carName) {
         if (!nameValidatePattern.matcher(carName).matches()) {
-            throw new IllegalArgumentException("한글, 영어, 숫자를 포함한 5자 이하의 이름을 , 구분하여 입력해주세요");
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NAMES.getMessage());
         }
     }
 
     private void validateCarNamesNumbers(List<String> carNames) {
         if (carNames.size() < CAR_NAMES_MIN_RANGE || carNames.size() > CAR_NAMES_MAX_RANGE) {
-            throw new IllegalArgumentException("2이상 20개 이하의 이름을 작성해 주세요.");
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NAMES_NUMBERS.getMessage());
+        }
+    }
+
+    private enum ErrorMessage {
+        INVALID_NAMES("특수문자를 제외한 5자 이하의 이름을 입력해주세요."),
+        INVALID_NAMES_NUMBERS("2이상 20개 이하의 이름을 작성해 주세요.");
+
+        private final String Message;
+
+        ErrorMessage(String message) {
+            Message = message;
+        }
+
+        public String getMessage() {
+            return Message;
         }
     }
 }
