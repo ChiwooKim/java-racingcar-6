ad79cca20567e11d6f3fb99b3863cc49f108a85f
kcw0360 <kcw0360@naver.com>
2023-11-20 19:04:57 +0900

    feat(Car): 차량 객체 작성

---
+++ src/main/java/racingcar/domain/car/Car.java
@@ -0,0 +1,15 @@
+package racingcar.domain.car;
+
+public class Car {
+
+    private String name;
+    private int position;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
